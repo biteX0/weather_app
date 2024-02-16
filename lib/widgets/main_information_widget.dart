ddda9ce9d2b089174692e2a47e295aca43d8d4c3
biteX0 <157722561+biteX0@users.noreply.github.com>
2024-02-16 11:29:13 +0500

    Update main_information_widget.dart

--- lib/widgets/main_information_widget.dart
+++ lib/widgets/main_information_widget.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+  var dayInfo = DateTime.now();
+  var dateFormat = DateFormate('EEEE, d MMM, yyyy').formate(dayInfo);
+
 class MainInformation extends StatelessWidget {
-  const MainInformation({Key? key}) : super(key: key);
+    const MainInformation({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +15,7 @@ class MainInformation extends StatelessWidget {
           child: Container(
             width: MediaQuery.sizeOf(context).width * 1,
             height: MediaQuery.sizeOf(context).height * 0.6,
+            pading: const EdgeInsets.only(top: 20),
             margin: const EdgeInsets.only(right: 10, left: 10),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(32),
@@ -23,6 +28,110 @@ class MainInformation extends StatelessWidget {
                 end: Alignment.topCenter,
                 stops: [0.2, 0.85],
               ),
+            ),
+            child: Column (
+              children[
+                Text(
+                  '${data?.cityName}',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.7),
+                    fontSize: 35,
+                  ),
+                  Text(
+                  dateFormat,
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.7),
+                    fontSize: 35,
+                  ),
+                    Image.asset(
+                      'assets/',
+                      width: size.width * 0.4,
+                    ),
+                    Text(
+                      'Sunny', 
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.9),
+                        fontSize: 15,
+                        fontWeight: FontWeaight.bold, 
+                      ),
+                      SizeBox(
+                        height: 5,
+                      ),
+                      Text(
+                  '${data?.temp}',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.7),
+                    fontSize: 75,
+                  ),
+                        SizeBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: 
+                              Column(
+                                Image.asset(
+                                  'assets/wind', 
+                                  width: size.width*0.15,
+                                ),
+                                Text(
+                  '${data?.wind} km/h',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.7),
+                    fontSize: 75,
+                  ),
+                                  Text(
+                  'wind',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.7),
+                    fontSize: 75,
+                  ),
+                                    Expanded(
+                              child: 
+                              Column(
+                                Image.asset(
+                                  'assets/humidity', 
+                                  width: size.width*0.15,
+                                ),
+                                Text(
+                  '${data?.humidity}',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.7),
+                    fontSize: 75,
+                  ),
+                                  Text(
+                  'Humidity',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.7),
+                    fontSize: 75,
+                  ),
+                                 Expanded(
+                              child: 
+                              Column(
+                                Image.asset(
+                                  'assets/windDir', 
+                                  width: size.width*0.15,
+                                ),
+                                Text(
+                  '${data?.windDir}',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.7),
+                    fontSize: 75,
+                  ),
+                                  Text(
+                  'Wind Direction',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.7),
+                    fontSize: 75,
+                  ),   
+                              ),
+                            ),
+                          ],
+                        ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
